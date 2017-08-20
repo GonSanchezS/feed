aa193f64df4ec468988d328dcd457d45c05f1658
Gonzalo <gonsanchezs@gmail.com>
2017-08-20 14:55:15 +0000

    Styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
   get 'all', to:'static_pages#all'
+  get 'submit', to:'static_pages#submit'
+  get 'almost', to:'static_pages#almost'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
